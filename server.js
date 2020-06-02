ba9f6068b4f031471277c1324f69fd3184b1f2aa
Aslihan <aslihanozfidan@gmail.com>
2020-06-02 22:00:57 +0300

    add cors

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = require("express");
-const routes = require("./routes");
-const app = express();
-const port = process.env.PORT || 2500;
-const router = new routes();
-const connection = require("./src/database/connection");
+const express = require('express')
+const routes = require('./routes')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 2500
+const router = new routes()
+const connection = require('./src/database/connection')
+
+app.use(cors({
+  origin: 'http://localhost:8080'
+}));
 
 appInitializer = () => {
-  connection.getInstance();
-  router.mountRoutes(app);
-};
+  connection.getInstance()
+  router.mountRoutes(app)
+}
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-appInitializer();
+appInitializer()
